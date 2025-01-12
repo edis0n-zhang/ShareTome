6ecb60d747893f969b96025dc55dd20b4646bffb
Edison Zhang <edisonzhang@ucsb.edu>
2025-01-12 08:25:48 -0800

    allow root

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -16,6 +16,6 @@ export default withAuth(
 export const config = {
   matcher: [
     "/api/:path*", // Protect all API routes
-    "/((?!api/auth|login|auth/error).*)",  // Protect all routes except auth-related ones
+    "/((?!api/auth|login|auth/error|$).*)", // Protect all routes except auth-related ones and root
   ],
 };
